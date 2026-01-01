08f046f48fe5954671d6fbfd85492e1927484dec
plutoniumstarfish <plutoniumstarfish@yahoo.com>
2026-01-01 20:57:14 +0800

    Save main.py.

--- ado_autosearch/main.py
+++ ado_autosearch/main.py
@@ -1,1 +1,9 @@
 import requests
+
+
+organization = ""
+project = ""
+
+#GET https://dev.azure.com/{organization}/{project}/_apis/git/repositories?api-version=4.1
+url_repo = "https://dev.azure.com/{organization}/{project}/_apis/git/repositories?api-version=4.1"
+
