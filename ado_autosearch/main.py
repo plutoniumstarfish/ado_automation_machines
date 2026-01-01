9cc4c163b0d9444fb9caf9e9b23ecf1896c82efe
plutoniumstarfish <plutoniumstarfish@yahoo.com>
2026-01-01 21:56:27 +0800

    Save main.py.

--- ado_autosearch/main.py
+++ ado_autosearch/main.py
@@ -1,8 +1,17 @@
 import requests
-
+from requests.auth import HTTPBasicAuth
 
 organization = "genex2anchetamax"
 project = "ado_automation_machines"
+
+PAT = ""
+
+auth = HTTPBasicAuth("", PAT)
+headers = {
+    "Content-Type": "application/json"
+}
+
+
 
 #GET https://dev.azure.com/{organization}/{project}/_apis/git/repositories?api-version=4.1
 url_repo = "https://dev.azure.com/{organization}/{project}/_apis/git/repositories?api-version=4.1"
